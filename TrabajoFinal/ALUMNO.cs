ccf85cac0eaf22cf8232bf8a94770f56ed9945d5
l.reyes <l.reyes@VDP-PROGOLF-01>
2025-03-20 11:11:15 -0400

    Agregar archivos de proyecto.

---
+++ TrabajoFinal/ALUMNO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoFinal
+{
+    class ALUMNO
+    {
+        public int IdAlumnos { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get;set;  }
+        public List  <float> Calificacion { get; set; }  
+
+    }
+}
---
+++ TrabajoFinal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TrabajoFinal
+{
+    internal class Program
+    {
+
+        public static void AgregarAlumnos()
+        {
+            List<string> Alumnos = new List<string>();
+            Alumnos.Add(Console.Read().ToString());
+
+            
+
+
+
+
+        }
+
+            
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===Sistema de Gestion Estudiantil===");
+            Console.WriteLine("\n");
+            Console.WriteLine("1. Agregar un nuevo estudiante");
+
+            Console.WriteLine("SElciones la opciones ");
+            int Opciones = int.Parse(Console.ReadLine());
+            switch (Opciones)
+            {
+                case ' 
+                    {
+
+                    }
+
+
+
+                default:
+            }
+
+        }
+    }
+}
